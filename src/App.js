de4014cfff44f40f2ec8a447389f15cc6f390120
Taranvir Hidkyar <thidkyar@gmail.com>
2020-05-25 17:29:07 -0400

    add routes for registation and homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
 import "./default.scss";
 
 function App() {
     return (
         <div className="App">
             <Header />
-
             <div className="main">
-                <Homepage />
+                <Switch>
+                    <Route exact path="/" component={Homepage} />
+                    <Route path="/registration" component={Registration} />
+                </Switch>
             </div>
         </div>
     );
---
+++ src/pages/Registration/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './styles.scss'
+import { render } from '@testing-library/react';
+
+class Registration extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    Registration page
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Registration
