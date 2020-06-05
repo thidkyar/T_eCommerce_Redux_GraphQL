58c7c2d51d163990c7441af3c40e746c56695dd7
Taranvir Hidkyar <thidkyar@gmail.com>
2020-06-05 18:05:46 -0400

    add footer/routes/layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
+
+//layouts
+import MainLayout from "./layouts/MainLayout";
+import HomepageLayout from "./layouts/HomepageLayout"
+
+//pages
 import Homepage from "./pages/Homepage";
 import Registration from "./pages/Registration";
 import "./default.scss";
@@ -8,13 +13,25 @@ import "./default.scss";
 function App() {
     return (
         <div className="App">
-            <Header />
-            <div className="main">
-                <Switch>
-                    <Route exact path="/" component={Homepage} />
-                    <Route path="/registration" component={Registration} />
-                </Switch>
-            </div>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    render={() => (
+                        <HomepageLayout>
+                            <Homepage />
+                        </HomepageLayout>
+                    )}
+                />
+                <Route
+                    path="/registration"
+                    render={() => (
+                        <MainLayout>
+                            <Registration />
+                        </MainLayout>
+                    )}
+                />
+            </Switch>
         </div>
     );
 }
---
+++ src/layouts/HomepageLayout.js
@@ -0,0 +1,17 @@
+//functional component
+
+import React from 'react'
+import Header from './../components/Header'
+import Footer from './../components/Footer'
+
+const HomepageLayout = props => {
+    return (
+        <div className="fullHeight">
+            <Header/>
+            {props.children}
+            <Footer />
+        </div>
+    );
+}
+
+export default HomepageLayout
