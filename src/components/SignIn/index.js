0c92e12f107d7903bd97a77dbfdc80517eb16718
Taranvir Hidkyar <thidkyar@gmail.com>
2020-06-07 22:31:05 -0400

    setup password reset page / refactor forms to reuseable component / layout changes

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+
 import "./styles.scss";
 import Button from "./../forms/Button";
 import { signInWithGoogle, auth } from "./../../firebase/utils";
 
+import AuthWrapper from "./../AuthWrapper";
 import FormInput from "./../forms/FormInput";
 
 const initialState = {
@@ -42,39 +45,44 @@ class SignIn extends Component {
 
     render() {
         const { email, password } = this.state;
-
+        const configAuthWrapper = {
+            headline: 'LogIn'
+        }
         return (
-            <div className="signin">
-                <div className="wrap">
-                    <h2>LogIn</h2>
-                    <div className="formWrap">
-                        <form onSubmit={this.handleSubmit}>
-                            <FormInput
-                                type="email"
-                                name="email"
-                                value={email}
-                                placeholder="Email"
-                                handleChange={this.handleChange}
-                            />
-                            <FormInput
-                                type="password"
-                                name="password"
-                                value={password}
-                                placeholder="Password"
-                                handleChange={this.handleChange}
-                            />
-                            <Button type="submit"> Login </Button>
-                            <div className="socialSignin">
-                                <div classname="row">
-                                    <Button onClick={signInWithGoogle}>
-                                        Sign in with Google
-                                    </Button>
-                                </div>
+            <AuthWrapper {...configAuthWrapper}>
+                <div className="formWrap">
+                    <form onSubmit={this.handleSubmit}>
+                        <FormInput
+                            type="email"
+                            name="email"
+                            value={email}
+                            placeholder="Email"
+                            handleChange={this.handleChange}
+                        />
+                        <FormInput
+                            type="password"
+                            name="password"
+                            value={password}
+                            placeholder="Password"
+                            handleChange={this.handleChange}
+                        />
+                        <Button type="submit"> Login </Button>
+                        <div className="socialSignin">
+                            <div className="row">
+                                <Button onClick={signInWithGoogle}>
+                                    Sign in with Google
+                                </Button>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+
+                        <div className="links">
+                            <Link to="/recovery">
+                                Reset Password
+                            </Link>
+                        </div>
+                    </form>
                 </div>
-            </div>
+            </AuthWrapper>
         );
     }
 }
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -3,6 +3,7 @@ import "./styles.scss";
 
 import { auth, handleUserProfile } from "./../../firebase/utils";
 
+import AuthWrapper from "./../AuthWrapper";
 import FormInput from "./../forms/FormInput";
 import Button from "./../forms/Button";
 
@@ -33,12 +34,7 @@ class Singup extends Component {
 
     handlFormSubmit = async (event) => {
         event.preventDefault();
-        const {
-            displayName,
-            email,
-            password,
-            confirmPassword,
-        } = this.state;
+        const { displayName, email, password, confirmPassword } = this.state;
 
         if (password !== confirmPassword) {
             const err = ["Passwords don't match"];
@@ -56,8 +52,8 @@ class Singup extends Component {
             await handleUserProfile(user, { displayName });
 
             this.setState({
-                ...initialState
-            })
+                ...initialState,
+            });
         } catch (err) {
             //console.log(err)
         }
@@ -71,12 +67,14 @@ class Singup extends Component {
             confirmPassword,
             errors,
         } = this.state;
+        
+        const configAuthWrapper = {
+            headline: 'Registration'
+        }
 
         return (
-            <div className="signup">
-                <div className="wrap">
-                    <h2>Signup</h2>
-
+            <AuthWrapper {...configAuthWrapper}>
+                <div className="formWrap">
                     {errors.length > 0 && (
                         <ul>
                             {errors.map((err, index) => {
@@ -84,49 +82,47 @@ class Singup extends Component {
                             })}
                         </ul>
                     )}
-                    <div className="formWrap">
-                        <form onSubmit={this.handlFormSubmit}>
-                            <FormInput
-                                type="text"
-                                name="displayName"
-                                value={displayName}
-                                placeholder="Full name"
-                                onChange={this.handleChange}
-                                required
-                            />
+                    <form onSubmit={this.handlFormSubmit}>
+                        <FormInput
+                            type="text"
+                            name="displayName"
+                            value={displayName}
+                            placeholder="Full name"
+                            onChange={this.handleChange}
+                            required
+                        />
 
-                            <FormInput
-                                type="email"
-                                name="email"
-                                value={email}
-                                placeholder="Email"
-                                onChange={this.handleChange}
-                                required
-                            />
+                        <FormInput
+                            type="email"
+                            name="email"
+                            value={email}
+                            placeholder="Email"
+                            onChange={this.handleChange}
+                            required
+                        />
 
-                            <FormInput
-                                type="password"
-                                name="password"
-                                value={password}
-                                placeholder="Password"
-                                onChange={this.handleChange}
-                                required
-                            />
+                        <FormInput
+                            type="password"
+                            name="password"
+                            value={password}
+                            placeholder="Password"
+                            onChange={this.handleChange}
+                            required
+                        />
 
-                            <FormInput
-                                type="password"
-                                name="confirmPassword"
-                                value={confirmPassword}
-                                placeholder="Confirm Password"
-                                onChange={this.handleChange}
-                                required
-                            />
+                        <FormInput
+                            type="password"
+                            name="confirmPassword"
+                            value={confirmPassword}
+                            placeholder="Confirm Password"
+                            onChange={this.handleChange}
+                            required
+                        />
 
-                            <Button type="submit">Register</Button>
-                        </form>
-                    </div>
+                        <Button type="submit">Register</Button>
+                    </form>
                 </div>
-            </div>
+            </AuthWrapper>
         );
     }
 }
